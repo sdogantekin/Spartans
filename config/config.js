a2878b12f3e80c8382ff4c2fde800459b8b83ca7
serkand <serkan.dogantekin@gmail.com>
2017-05-14 15:47:38 +0300

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,16 @@
+var config = {};
+
+config.env = "DEV";
+
+if (config.env == "DEV") {
+    config.mongo     = {};
+    config.mongo.url = "mongodb://spartans:serkan123@ds151279.mlab.com:51279/spartans";
+
+    config.log       = {};
+    config.log.level = "debug";
+
+    config.mongoose       = {};
+    config.mongoose.debug = true;
+}
+
+module.exports = config;
---
+++ passport/setup.js
@@ -0,0 +1,44 @@
+// The most widely used way for websites to authenticate users is via a username and password.
+// Support for this mechanism is provided by the passport-local module
+var passport      = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var User          = require("../models/user");
+
+// the following function will be used to initialize passport configuration
+module.exports = function() {
+    // Passport will serialize and deserialize user instances to and from the session
+    passport.serializeUser(function(user,done){
+        // first parameter is error
+        // mongo documents has an unique _id field
+        done(null, user._id);
+    });
+    passport.deserializeUser(function(id,done){
+        User.findById(id,function(err,user){
+            done(err,user);
+        });
+    });
+    // By default, LocalStrategy expects to find credentials in parameters named username and password
+    // If your site prefers to name these fields differently, options are available to change the defaults
+    passport.use("spartans",new LocalStrategy(
+        function(username,password,done) {
+            User.findOne({username:username},function(err,user){
+                if(err) {
+                    return done(err);
+                }
+                if(!user) {
+                    return done(null,false,{message: "No suitable user found"});
+                }
+                user.validatePassword(password,function(err,isMatch){
+                    if(err) {
+                        return done(err);
+                    }
+                    if(isMatch) {
+                        return done(null,user);
+                    } else {
+                        return done(null,false,{message: "No suitable user found"});
+                    }
+                });
+            });
+        }
+    ));
+};
