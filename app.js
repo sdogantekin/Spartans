2c24e5c1e64dbc76d6d0d1dbe34a490fb12e93d9
Gulsah <gulsah.suluk@gmail.com>
2017-05-24 12:17:03 +0300

    add flash messsage type

--- app.js
+++ app.js
@@ -69,6 +69,14 @@ app.use(expressValidator({
     }
 }));
 
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
