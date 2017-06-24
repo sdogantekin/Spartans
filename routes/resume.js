4bb18a9dad9df87385b5ed30e2c4996994125c17
serkand <serkan.dogantekin@gmail.com>
2017-06-25 02:39:38 +0300

    starting point for saving a new candidate information using a json based ajax call

--- routes/resume.js
+++ routes/resume.js
@@ -7,66 +7,28 @@ router.get("",function(request,response,next){
 });
 
 router.post("", function (request, response, next) {
-
-    var name = request.body.name;
-    var surname = request.body.surname;
-    var contactInfo = request.body.contactInfo;
-    var birthDate = request.body.birthDate;
-    var experience = request.body.experience;
-    var language = request.body.language;
-    var categories = request.body.categories;
-    var skills = request.body.skills;
-    var location = request.body.location;
-    var workName = request.body.workName;
-    var workTitle = request.body.workTitle;
-    var workStart = request.body.workStart;
-    var workEnd = request.body.workEnd;
-    var educationSchool = request.body.educationSchool;
-    var educationDegree = request.body.educationDegree;
-    var educationStart = request.body.educationStart;
-    var educationEnd = request.body.educationEnd;
-    var salaryMin = request.body.salaryMin;
-    var salaryMax = request.body.salaryMax;
-    var userId = request.user._id;
-
-    var newResume = new Resume({name: name,
-        surname: surname,
-        contactInfo: contactInfo,
-        birthDate: birthDate,
-        experience: experience,
-        language: language,
-        categories: categories,
-        skills: skills,
-        location: location,
-        workHistory: {
-        name : workName,
-            title : workTitle,
-            start : workStart,
-            end : workEnd
-        },
-        educationHistory: {
-        school : educationSchool,
-            degree : educationDegree,
-            start : educationStart,
-            end : educationEnd
-        },
-        salary : {
-        min: salaryMin,
-            max: salaryMax
-        },
-        userId : userId
-
+    var newResume = new Resume({
+        name:    request.body.name,
+        surname: request.body.surname,
+        email:   request.body.email,
+        phone:   request.body.phone,
+        language:request.body.language,
+        userId : request.user._id
     });
 
     newResume.save(function(err){
         if(!err){
-            return response.redirect('/users/' + request.user.username);
-        }
-        else{
-            return response.send("Error");
+            var result = {};
+            result.status = "success";
+            return response.json(result);
+        } else{
+            var result     = {};
+            result.status  = "fail";
+            result.message = err;
+            return response.json(result);
         }
     });
-})
+});
 
 router.get("/getResume",function(request,response,next){
     Resume.findResume(request.user._id, function (res) {
