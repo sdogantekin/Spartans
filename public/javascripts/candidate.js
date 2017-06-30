da80fa4e3bafa93f83fc0928c22fb76c544cd75a
Gulsah <gulsah.suluk@gmail.com>
2017-06-30 22:21:48 +0300

    added candidate html manipulation

--- public/javascripts/candidate.js
+++ public/javascripts/candidate.js
@@ -1,6 +1,7 @@
 /**
  * Created by serkand on 20/05/2017.
  */
+
 $.ajaxSetup({complete: onRequestCompleted});
 
 function onRequestCompleted(xhr,textStatus) {
@@ -11,15 +12,58 @@ function onRequestCompleted(xhr,textStatus) {
 
 function saveResume() {
     var resume = {};
-    resume.name     = $("#resumeBasics #name").val();
-    resume.surname  = $("#resumeBasics #surname").val();
-    resume.email    = $("#resumeBasics #email").val();
-    resume.phone    = $("#resumeBasics #phone").val();
-    resume.language = [];
+    resume.name      = $("#resumeBasics #name").val();
+    resume.surname   = $("#resumeBasics #surname").val();
+    resume.email     = $("#resumeBasics #email").val();
+    resume.phone     = $("#resumeBasics #phone").val();
+    resume.birthDate = $("#resumeBasics #birthDate").val();
+    resume.language  = [];
     $("#resumeLanguage #language").each(function (index) {
         var $this = $(this);
         resume.language.push($this.text());
     });
+
+    //Work History and Education info will be added
+
+    resume.categories = [];
+    $('#resumeExpert li').each(function()
+    {
+        if($(this).hasClass('active')){
+            resume.categories.push($(this).text())
+        }
+    });
+    resume.skills = [];
+    $('#resumeSkill li').each(function () {
+        if($(this).hasClass('active')){
+            resume.skills.push($(this).text())
+        }
+    });
+    $('#resumeLocation li').each(function () {
+        if($(this).hasClass('active')){
+            resume.location = $(this).text();
+        }
+    });
+
+    resume.preferredLocation = [];
+    $('#resumePreferLocation li').each(function () {
+        if($(this).hasClass('active')){
+            resume.preferredLocation.push($(this).text());
+        }
+    });
+
+    debugger;
+
+    var salary = {}
+
+    salary.min = $("#resumeSalary #minSalary").val();
+    salary.max = $("#resumeSalary #maxSalary").val();
+
+    resume.salary = salary;
+
+    resume.other = [];
+    $('#resumeOther li').each(function () {
+        resume.other.push($(this).text())
+    })
 
     $.ajax({
         url: "/resume",
@@ -40,36 +84,36 @@ $(document).ready(function() {
 
     //Add Expert
     $(document).on('keypress', '#inputExpert', function(e) {
-
+        var id = $("#inputExpert").val()
         if ( e.keyCode == 13 ) {  // detect the enter key
-            $("#resumeExpert").append($("<li id=\"expert\" class='active'>").text($("#inputExpert").val()));
+            $("#resumeExpert").append($("<li id="+id+" class='active' onclick='changeStatus(id)'>").text($("#inputExpert").val()));
         }
 
     });
 
     //Add Skill
     $(document).on('keypress', '#inputSkill', function(e) {
-
+        var id = $("#inputSkill").val()
         if ( e.keyCode == 13 ) {  // detect the enter key
-            $("#resumeSkill").append($("<li id=\"skill\" class='active'>").text($("#inputSkill").val()));
+            $("#resumeSkill").append($("<li id="+id+"   class='active' onclick='changeStatus(id)'>").text($("#inputSkill").val()));
         }
 
     });
 
     //Add Location
     $(document).on('keypress', '#inputLocation', function(e) {
-
+        var id = "n" + $("#inputLocation").val()
         if ( e.keyCode == 13 ) {  // detect the enter key
-            $("#resumeLocation").append($("<li id=\"location\" class='active'>").text($("#inputLocation").val()));
+            $("#resumeLocation").append($("<li id="+id+"  class='active' onclick='changeStatus(id)'>").text($("#inputLocation").val()));
         }
 
     });
 
     //Add Prefer Location
     $(document).on('keypress', '#inputPreferLocation', function(e) {
-
+        var id = $("#inputPreferLocation").val()
         if ( e.keyCode == 13 ) {  // detect the enter key
-            $("#resumePreferLocation").append($("<li id=\"preferLocation\" class='active'>").text($("#inputPreferLocation").val()));
+            $("#resumePreferLocation").append($("<li id="+id+"  class='active' onclick='changeStatus(id)'>").text($("#inputPreferLocation").val()));
         }
 
     });
@@ -85,3 +129,16 @@ function addOther(){
 
 }
 
+function changeStatus(id){
+    if($("#" + id).hasClass('active')){
+        $("#" + id).removeClass('active')
+    }
+    else{
+        $("#" + id).attr('class', 'active')
+    }
+}
+
+//Jquery UI Date Picker
+$( function() {
+    $( "#birthDate" ).datepicker();
+} );
